569ba88883d3a8191750909c58c25da51395261d
DuanHl <duanhl@duanhaoliangdeMacBook-Pro.local>
2018-03-13 18:58:17 +0800

    feature

--- app/src/main/java/com/fanqiecar/demo/api/Api.java
+++ app/src/main/java/com/fanqiecar/demo/api/Api.java
@@ -7,6 +7,7 @@ import com.fanqiecar.demo.model.Location;
 import com.fanqiecar.demo.model.RespData;
 import com.fanqiecar.demo.model.ShareWords;
 import com.fanqiecar.demo.model.UploadResult;
+import com.fanqiecar.demo.network.NetService;
 
 import io.reactivex.Completable;
 import io.reactivex.Observable;
@@ -26,7 +27,7 @@ import retrofit2.http.Url;
 
 public interface Api {
     //http://ip.taobao.com/service/getIpInfo.php?ip=63.223.108.42
-    @Headers(NetClient.HEADER_DOMAIN_PREFIX + Domain.TAOBAO)
+    @Headers(NetService.HEADER_DOMAIN_PREFIX + Domain.TAOBAO)
     @GET("/service/getIpInfo.php")
     Observable<RespData<AreaOfIP>> queryIp(@Query("ip") String ip);
 
@@ -35,16 +36,16 @@ public interface Api {
     Observable<Data> queryIp1();
 
     //http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=218.4.255.255
-    @Headers(NetClient.HEADER_DOMAIN_PREFIX + Domain.SINA)
+    @Headers(NetService.HEADER_DOMAIN_PREFIX + Domain.SINA)
     @GET("/iplookup/iplookup.php?format=json")
     Observable<City> queryIp2(@Query("ip") String ip);
 
     //http://gc.ditu.aliyun.com/regeocoding?l=39.938133,116.395739&type=001
-    @Headers(NetClient.HEADER_DOMAIN_PREFIX + Domain.ALIYUN)
+    @Headers(NetService.HEADER_DOMAIN_PREFIX + Domain.ALIYUN)
     @GET("/regeocoding?l=39.938133,116.395739&type=001")
     Observable<Location> queryLocation();
 
-    @Headers(NetClient.HEADER_DOMAIN_PREFIX + Domain.REST)
+    @Headers(NetService.HEADER_DOMAIN_PREFIX + Domain.REST)
     @GET("/share/words")
     Observable<RespData<ShareWords>> getShareWords();
 
